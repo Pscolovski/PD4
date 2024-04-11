e1bc8607b6e27ea6c86291865c225919c64c1faf
Pscolovski <157747460+Pscolovski@users.noreply.github.com>
2024-04-11 11:22:01 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let petName;
+let petAge;
+
+petName = "Rex";
+petAge = 5;
+
+console.log(petName);
+console.log(petAge);
+
+petName = "Buddy";
+petAge = 7;
+
+const buildingAddress = "123 Main Street";
+
+console.log(buildingAddress);
